c994e576b5c21a1bbf17c544672c39fb7f21f305
rihem1999 <rihem.farjallah1999@gmail.com>
2023-09-07 16:19:55 +0100

    tex

--- front-end/src/components/PDFViewer.js
+++ front-end/src/components/PDFViewer.js
@@ -8,6 +8,8 @@ const PDFViewer = ({ file }) => {
   const [pageNum, setPageNum] = useState(1);
   const [scale, setScale] = useState(1);
   const [isSelecting, setIsSelecting] = useState(false);
+  const [selectedText, setSelectedText] = useState('');
+
   const [selectionRect, setSelectionRect] = useState({
     x: 0,
     y: 0,
@@ -130,34 +132,43 @@ const PDFViewer = ({ file }) => {
     // Convert the screenshot canvas to a data URL (base64)
     const screenshotDataURL = screenshotCanvas.toDataURL('image/png');
   
-    // Now you can use screenshotDataURL as an image source or save it as needed
-    console.log('Screenshot Data URL:', screenshotDataURL);
-    // Send the screenshot to the backend
-    fetch('/upload-screenshot', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ screenshot: screenshotDataURL }),
-    })
-      .then((response) => {
-        if (response.ok) {
-          console.log('Screenshot uploaded successfully');
-          // Handle success as needed
-        } else {
-          console.error('Failed to upload screenshot');
-          // Handle error as needed
+    // Use PDF.js to extract text from the selected area
+    pdfDoc.getPage(pageNum).then((page) => {
+      page.getTextContent().then((textContent) => {
+        const textItems = textContent.items;
+        const selectedTextItems = [];
+  
+        for (const textItem of textItems) {
+          const itemX = textItem.transform[4];
+          const itemY = page.getViewport({ scale }).height - textItem.transform[5]; // Adjust Y-coordinate
+  
+          if (
+            itemX >= x &&
+            itemX <= x + width &&
+            itemY >= y &&
+            itemY <= y + height
+          ) {
+            selectedTextItems.push(textItem.str);
+          }
         }
-      })
-      .catch((error) => {
-        console.error('Error while uploading screenshot:', error);
-        // Handle error as needed
+  
+        const selectedText = selectedTextItems.join(' ');
+        setSelectedText(selectedText);
+        console.log('Selected Text:', selectedText);
+  
+        // Send the selected text to the backend or use it as needed
+        // ...
       });
-    
+    });
+  
     // Reset the selection rectangle
     setSelectionRect({ x: 0, y: 0, width: 0, height: 0 });
   };
+  
+  
 
+  
+  
   return (
     <div className="pdf-container" ref={pdfContainerRef} style={{margin: '20px'}}>
       <canvas
@@ -183,6 +194,10 @@ const PDFViewer = ({ file }) => {
         <span style={{marginLeft: 20}}>Page {pageNum}</span>
         <button style={{marginLeft: 20}}onClick={goToNextPage} disabled={pageNum === (pdfDoc ? pdfDoc.numPages : 1)}>Next Page</button>
       </div>
+      <div className="selected-text">
+  <strong>Selected Text:</strong> {selectedText}
+</div>
+
     </div>
   );
 };
