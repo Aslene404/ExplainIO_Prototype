204422aa36fa1272ccfe9dcc58a6baa70dd616bb
Aslene <aslenerazor@gmail.com>
2023-09-13 15:54:07 +0100

    CI/CD test

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -53,7 +53,7 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Hello Again! and raqwave</h1>
+      <h1>Hello its 15:53</h1>
       <p>{message}</p>
       <button onClick={fetchData}>Fetch Data</button>
       {data ? (
